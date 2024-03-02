8968f94bc5ad82758e7c92660a17a62f7cb382be
Andrew Hunsaker <ahuns2@byu.edu>
2024-03-01 23:42:30 -0700

    Edit and Delete

--- Mission08_Team0310/Controllers/HomeController.cs
+++ Mission08_Team0310/Controllers/HomeController.cs
@@ -35,12 +35,57 @@ namespace Mission08_Team0310.Controllers
             return RedirectToAction("Index"); // Redirect to another action, such as Index
         }
 
-
         [HttpGet]
         public IActionResult AddTasks()
         {
             ViewBag.Categories = _context.Categories.ToList();
             return View();
+        }
+
+        public IActionResult Quadrants()
+        {
+            var ToDoItems = _context.ToDoItems
+                .Where(x => x.Completed == false)
+                .ToList();
+
+            return View(ToDoItems);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var recordToEdit = _context.ToDoItems
+                .Single(x => x.TaskId == Id);
+
+            ViewBag.Categories = _context.Categories.ToList();
+            return View("AddTasks", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ToDoItem updatedInfo)
+        {
+            _context.Update(updatedInfo);
+            _context.SaveChanges();
+
+            return RedirectToAction("Quadrants");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var recordToDelete = _context.ToDoItems
+                .Single(x => x.TaskId == Id);
+
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ToDoItem task)
+        {
+            _context.ToDoItems.Remove(task);
+            _context.SaveChanges();
+
+            return RedirectToAction("Quadrants");
         }
     }
 }
